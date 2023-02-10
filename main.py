b43b19fba6b147bd66028153ef305a7ec717cd74
Abhishek Bhovar <abhibhovar@gmail.com>
2023-02-11 00:27:14 +0530

    Code with harry completed till 40mins

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 import requests
 from bs4 import BeautifulSoup
 
@@ -23,29 +24,63 @@ if resp.status_code >= 200 and resp.status_code < 400:
     # the content we recieve in response is in byte format, which is of no use to us in the following process we will convert the bytes in a more useful structure which is a "tree"
     # this process of converting one format to another is called parsing, and yes obviously we will require a parser to do so
     
-    # print(html_content)
-    
     # parse the HTML content
     # for parsing the bytes content in more useful one we will use the html_parser provided by the bs4 module
     tree_str = BeautifulSoup(byte_content, 'html.parser')
-    print(tree_str.prettify()) # just for printing the content in proper manner
-
+    # print(tree_str.prettify()) # just for printing the content in proper manner
 
     # traversing through the tree
     # getting the title of the HTML Page
     title = tree_str.title # following attribute is a Tag-type object in the tree-str
     # Most commonly use objects
-    print(type(tree_str))               # 3. BeautifulSoup
-    print(type(title))                  # 1. Tag
-    print(type(tree_str.string))        # 2. Navigable String
+    # print(type(tree_str))               # 3. BeautifulSoup
+    # print(type(title))                  # 1. Tag
+    # print(type(tree_str.string))        # 2. Navigable String
     # 4. Comment
 
-
     # searching for elements in the page/tree
+    # find all method is used to find all the elements from the page with passed tag
     # eg: finding all the paragraphs
     paras = tree_str.find_all('p')
 
+    # finding all the anchors from the page
+    anchors = tree_str.find_all('a')
+        
+    # print(type(paras))                  # 4. ResultSet : just a list containing multiple objects provided by BeautifulSoup
+    # print(paras)
+    # print(paras[0])                     # Tag-type
 
+    # if instead of "find_all" we use "find" function we will get the very first element instead of all
+    # print(tree_str.find('p'))
+    first_para = tree_str.find('p') # this will return the very first para it will find in the 
+    # print(first_para['class']) # following statement will get all the classes in the first_para
+    # print(first_para)
+    # print(first_para.get_text()) # this will print the text inside the following tag, with tag or styling we will get the plain text
+
+    # querying on the page
+    # finding all the elements with class "text-sm"
+    p_text_sm = tree_str.find_all("p", class_="text-sm") # to find all
+    # p_text_sm = tree_str.find("p", class_="text-sm") # to find only the first one
+    # print(p_text_sm)
+    actual_links = set()
+    # getting all the links from the page
+    for link in anchors:
+        if link != "#" and link != "/" and "http" not in link.get('href'):
+            # print(link.get('href')) # following is an tag object we can use the get method to get any of the attribute from the element
+            # links provided will not be the whole url
+            # for that we need to add the base url before the link
+            actual_links.add(f"{url.rstrip('/')}{link.get('href')}")
+    # pprint(actual_links)
+
+    # searching based on id
+    element_by_id = tree_str.find(id = "<id we want to search by>")
+
+
+    # for elements having nested element, i.e. a <p> having <div> inside of it and so on
+    # we can iterate through the content of the parent to find the child element inside of it
+    # for elem in paras[1].content:
+    #     print(elem)
+    # print(type(paras[0].find_parents()))
 
 
 # closing the connection of the response object
